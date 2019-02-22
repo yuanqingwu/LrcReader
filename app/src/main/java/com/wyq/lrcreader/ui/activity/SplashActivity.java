e1818d9d4804996778d6e187813f9333d7827177
wuyuanqing <wuyuanqing527@gmail.com>
2019-02-22 16:47:57 +0800

    优化UI：增加闪屏页，增加activity进入方式

---
+++ app/src/main/java/com/wyq/lrcreader/ui/activity/SplashActivity.java
@@ -0,0 +1,16 @@
+package com.wyq.lrcreader.ui.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_splash);
+        HomeActivity.newInstance(this);
+        finish();
+    }
+}
